20cdef652d96ccb1425b0c2902796ec1935bb077
liva <liva.ok@gmail.com>
2021-12-08 17:53:48 +0300

    add 'END' in output

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 public class TestApplication {
     public static void main(String[] args) {
-        System.out.println("START!");
+        System.out.println("START");
+        System.out.println("END");
     }
 }
