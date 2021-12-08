81f809c8e1b3bd18017ab9f3ea971be5a8c47237
liva <liva.ok@gmail.com>
2021-12-08 17:45:41 +0300

    add '!' in out

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -2,6 +2,6 @@ package com.example.test;
 
 public class TestApplication {
     public static void main(String[] args) {
-        System.out.println("START");
+        System.out.println("START!");
     }
 }
